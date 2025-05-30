7159edd702383afe92373ac0044dfe05b5f30356
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2025-05-30 14:16:53 +0100

    it did not work
    the   def that meant to reconnect every now and then dose not work :(

--- sql_login.py
+++ sql_login.py
@@ -9,39 +9,35 @@ def Error(error_has_happen):
    @discord_items.The_bot.event
    async def on_ready():
       await send_error_message()
+   
    discord_items.The_bot.run(discord_items.The_key)
 
 
 async def send_error_message():
     time = datetime.datetime.now()
-    chennels = discord_items.The_bot.get_channel(1330313688221220864)
+    #you will need to change the channel ID to the one that you want the bot to send the error message to
+    chennels = discord_items.The_bot.get_channel(1330313688221220864) 
     await chennels.send(f"I was Unable to connect to the Sql Server, at {time.now()}\n the file has been updated with the error that has occurred ")
-
-@discord_items.tasks.loop(seconds=1500)
-async def Reconnect():
-  # This function will try to reconnect to the database every 1500 seconds (25 minutes)
-  # just in case something went wrong with the connection
-   if error_has_happen == True:
-     Login()
-   else:
-     Reconnect.cancel()
-def Login():
- try:
-     db = mysql.connector.connect(
-        host = "Localhost",
-        user = "root",
-        password = "",
-        database = "discor",
-        autocommit=True
-       )
-       
- except:
-     error_has_happen = True
-     time = datetime.datetime.now()
-     print(f"Unable to Connect To The Server, on {time.now()}")
-     with open("errorMessages.txt", "a") as error_file:
-           error_file.write(f"\nUnable to Connect To The Server, on {time.now()}")
-     Error(error_has_happen)
     
 
-Login()
+
+try:
+    #you will need to change the host, user, password and database to the one that you are using on your sql server
+    db = mysql.connector.connect(
+       host = "Localhost",
+       user = "root",
+       password = "",
+       database = "discord",
+       autocommit=True
+      )
+    print("Connected To The Server Successfully")
+      
+except:
+    error_has_happen = True
+    time = datetime.datetime.now()
+    print(f"Unable to Connect To The Server, on {time.now()}")
+    with open("errorMessages.txt", "a") as error_file:
+          error_file.write(f"\nUnable to Connect To The Server, on {time.now()}")
+    Error(error_has_happen)
+    
+
