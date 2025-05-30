f9780c60e80985e633a9d39a78a2f4836032e7c6
ErrorNotJoin <86501620+errornotjoin@users.noreply.github.com>
2025-05-30 13:34:41 +0100

    reconnect
    add where the bot try to reconnect to the server every 25min and readded the .gitignore file

--- sql_login.py
+++ sql_login.py
@@ -17,21 +17,31 @@ async def send_error_message():
     chennels = discord_items.The_bot.get_channel(1330313688221220864)
     await chennels.send(f"I was Unable to connect to the Sql Server, at {time.now()}\n the file has been updated with the error that has occurred ")
 
-try:
-    db = mysql.connector.connect(
-       host = "Localhost",
-       user = "root",
-       password = "",
-       database = "discord",
-       autocommit=True
-      )
-      
-except:
-    error_has_happen = True
-    time = datetime.datetime.now()
-    print(f"Unable to Connect To The Server, on {time.now()}")
-    with open("errorMessages.txt", "a") as error_file:
-          error_file.write(f"\nUnable to Connect To The Server, on {time.now()}")
-    Error(error_has_happen)
-   
+@discord_items.tasks.loop(seconds=1500)
+async def Reconnect():
+  # This function will try to reconnect to the database every 1500 seconds (25 minutes)
+  # just in case something went wrong with the connection
+   if error_has_happen == True:
+     Login()
+   else:
+     Reconnect.cancel()
+def Login():
+ try:
+     db = mysql.connector.connect(
+        host = "Localhost",
+        user = "root",
+        password = "",
+        database = "discor",
+        autocommit=True
+       )
+       
+ except:
+     error_has_happen = True
+     time = datetime.datetime.now()
+     print(f"Unable to Connect To The Server, on {time.now()}")
+     with open("errorMessages.txt", "a") as error_file:
+           error_file.write(f"\nUnable to Connect To The Server, on {time.now()}")
+     Error(error_has_happen)
+    
 
+Login()
